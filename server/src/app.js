524e94d64406e0d9e4371a368182142b10f132f1
sean-akh <sean6projects@gmail.com>
2018-08-08 21:56:55 -0700

    git

--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,25 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import chalk from 'chalk';
+<<<<<<< HEAD
 import sequelize from 'sequelize';
 import db from './models';
 
+=======
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import userRouter from './routes/user-router';
+import './auth/passport';
+>>>>>>> master
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// not sure if we actually need this
+app.use(cookieParser());
+
 app.use(logger('dev'));
 app.use(cors({
   allowedHeaders: 'Content-Type, authorization',
@@ -35,18 +46,49 @@ app.get('/api/hello', (req, res) => {
   res.send(JSON.stringify({ word: 'Hello world' }));
 });
 
+// for the protected route '/user' to go through the passport middleware
+// that allows only requests with valid tokens to access
+// TODO: replace the following with the code below:
+/**
+ * // in this file
+ * app.use(/protected, protectedRouter);
+ *
+ *
+ * // in /routes/protectedRouter.js
+ *
+ * import { Router } from 'express';
+ * import userRouter from './user-router';
+ * const protectedRouter = Router();
+ * router.route('/user', userRouter);
+ */
+app.use('/user', passport.authenticate('jwt', { session: false }), userRouter);
+
+
 app.use(express.static(path.join(__dirname, `/../../${staticPathOffset}client/build/`)));
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, `/../../${staticPathOffset}client/build/`, 'index.html'));
 });
 
-// // db.sequelize.sync().then(function () {
-// //   console.log('sequelize is working');
-// app.listen(process.env.PORT, () => {
-//   console.log(chalk.blue('listening on port:', chalk.green.bold(process.env.PORT)));
-// });
-// // });
+// TODO: look into the following
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 app.listen(process.env.PORT, () => {
   console.log(chalk.blue('listening on port:', chalk.green.bold(process.env.PORT)));
